f3a3a639e03b9ac494d990ba048acda974b628ad
Tokito <93600306+Tokito-Kun@users.noreply.github.com>
2023-08-19 07:59:46 +0530

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -136,7 +136,7 @@ get-env-vars() {
 build (){
   get-base-pkg "$PACKAGE"
   # Build Package
-  cd "/home/user/$AUR_PKG" || pr "$AUR_PKG Repo doesn't exist" && exit 1
+  cd "/home/user/$AUR_PKG" || exit 1
   export USE_CCACHE=1
   export CCACHE_EXEC=/usr/bin/ccache
   ccache -M "$CCACHE_SIZE"
