85ac904d08c77b8b66addb36e060037135e90d20
Tokito-Kun <93600306+Tokito-Kun@users.noreply.github.com>
2023-09-04 23:28:14 +0530

    [Feature] just use yay
    for advance dependence resolving use yay

--- build.sh
+++ build.sh
@@ -4,33 +4,26 @@ source utils.sh
 source config.conf
 
 print_usage() {
-	echo -e "Usage:\n${0} build|setupenv|checkpkg|buildeps|clean|logs"
+	echo -e "Usage:\n${0} clean|setupenv|checkpkg|build|logs"
 }
 
 if [ -z ${1+x} ]; then
 	print_usage
 	exit 0
 elif [ "$1" = "clean" ]; then
-	rm -rf "/home/$NR_USER/$PACKAGE"
+	rm -rf "/home/user/build/"
+	exit 0
+elif [ "$1" = "setupenv" ]; then
+	setupenv
+	add-nroot-user
 	exit 0
 elif [ "$1" = "checkpkg" ]; then
 	check-pkg
 	exit 0
-elif [ "$1" = "setupenv" ]; then
-	setupenv
-	add-nroot-user "$NR_USER"
-	exit 0
-elif [ "$1" = "buildeps" ]; then
-	cd "/home/$NR_USER" || exit 1
-	clone-repo "$PACKAGE"
-  get-env-vars
-	ci-depends "$PACKAGE"
-	exit 0
 elif [ "$1" = "build" ]; then
-	verify-source
 	build
-	get-packages
-	pr "Package : $PACKAGE Built Successfuly"
+	get-env-vars
+	get-packages && pr "Package : $PACKAGE Built Successfuly"
 	exit 0
 elif [ "$1" = "logs" ]; then
 	get-logs
--- utils.sh
+++ utils.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
 source custom_package_list.sh
 
 pr() { echo -e "\033[0;32m[+] ${1}\033[0m"; }
-
-log() { echo -e "$1  " >>"$2"; }
 
 setupenv() {
   # Pacman Config
@@ -30,104 +29,51 @@ setupenv() {
 }
 
 add-nroot-user() {
-  local USER=$1
   # Add a non-root user (makepkg doesn't accept root)
   groupadd sudo
-  useradd -m "$USER" || true
-  usermod -aG sudo "$USER"
-  echo "## Allow $USER to execute any root command
+  useradd -m user || true
+  usermod -aG sudo user
+  echo "## Allow user to execute any root command
   %user ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> "/etc/sudoers"
 }
 
-get-base-pkg() {
-  local PACKAGE=$1
-  AUR_PKG=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].PackageBase')
+install-yay() {
+  cd /home/user/ || exit 1
+  sudo -u user bash <<EXC
+  git clone "https://aur.archlinux.org/yay.git"
+  cd yay || exit 1
+  makepkg -Csi --noconfirm --needed
+EXC
 }
 
 check-pkg() {
-  CHECK_REPO=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r ".resultcount")
-  if [ "$CHECK_REPO" -eq 0 ];then
-    pr "$PACKAGE Package not found in AUR Repo"
+  local AUR_PKG=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].PackageBase');
+  if [ "$AUR_PKG" = null ];then
+    pr "$PACKAGE Package not found in AUR Repo || Abort"
+    exit 1
   else
     pr "$PACKAGE Package is Available Continue to Build"
+    install-yay
   fi
 }
 
-clone-repo() {
-  local PACKAGE=$1
-  # Clone repo
-  get-base-pkg "$PACKAGE"
-  check-queue && PKGQ="$PKGQ $AUR_PKG"
-  if [ "$SCLONE" = false ];then
-    pr "Package $AUR_PKG Already in Queue"
-  else
-    git clone "https://aur.archlinux.org/$AUR_PKG.git"
-    chown -R "$NR_USER":"$NR_USER" "$AUR_PKG"
-    cd "$AUR_PKG" || exit 1
-  fi
-}
-
-build-depends() { sudo -u "$NR_USER" makepkg -Csi --noconfirm --needed; }
-
-verify-source() { sudo -u "$NR_USER" makepkg -Cs --verifysource --noconfirm --needed; }
-
-build-package() { sudo -u "$NR_USER" makepkg -CLs --noconfirm --needed; }
-
-get-depends() {
-  local PACKAGE=$1
-  # Get Depends
-  MDEPS=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].MakeDepends[]' | sed 's/"//g' | sed 's/>=[^"]*//g' | tr '\n' ' ')
-  DEPS=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].Depends[]' | sed 's/"//g' | sed 's/>=[^"]*//g' | tr '\n' ' ')
-  ODEPS=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].OptDepends[]' | sed 's/"//g' | sed 's/>=[^"]*//g' | tr '\n' ' ')
-  if [ "$2" = "SUB" ];then
-    SUB_PACKDEPS="${MDEPS} ${DEPS} ${ODEPS}"
-  else
-    PACKDEPS="${MDEPS} ${DEPS} ${ODEPS}"
-  fi
-}
-
-check-broken-packages() {
-  local DEPENDS=$1
-  if [ "$2" = "SUB" ];then
-    if [[ -n ${broken[$SUBDEPENDS]} ]]; then SUB_PACKDEPNDS="${broken[$SUBDEPENDS]}"; else SUB_PACKDEPNDS="$SUBDEPENDS"; fi
-  else
-    if [[ -n ${broken[$DEPENDS]} ]]; then PACKDEPNDS="${broken[$DEPENDS]}"; else PACKDEPNDS="$DEPENDS"; fi
-  fi
-}
-
-check-package-availability() {
-  local PACKDEPNDS=$1
-  if ! pacman -Si "$PACKDEPNDS" &> /dev/null; then
-    killall pacman 2>/dev/null
-    pr "pacman: target not found: $PACKDEPNDS" && pr "Checking For $PACKDEPNDS In AUR Repo"
-    CHECK_REPO=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKDEPNDS" | jq -r ".resultcount")
-    if [ "$CHECK_REPO" -eq 0 ];then pr "$PACKDEPNDS Package not found in AUR Repo Too" && exit 1
-      else
-      pr "$PACKDEPNDS Package found in AUR Repo | build it!"
-      clone-repo "$PACKDEPNDS"
-    fi
-  fi
-}
-
-check-queue(){
-  for package in $PKGQ; do
-    if [ "$package" = "$AUR_PKG" ];then SCLONE=false; fi
-  done
-}
-
-ci-depends() {
-  local PACKAGE="$1"
-  get-depends "$PACKAGE"
-  for DEPENDS in $PACKDEPS; do
-    check-broken-packages "$DEPENDS"
-    check-package-availability "$PACKDEPNDS"
-    ci-depends "$PACKDEPNDS"
-    build-depends
-  done
+build (){
+  cd "/home/user/" || exit 1
+  local AUR_PKG=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].PackageBase');
+  # Build Package
+  export USE_CCACHE=1
+  export CCACHE_EXEC=/usr/bin/ccache
+  ccache -M "$CCACHE_SIZE"
+  sudo -u user bash <<EXU
+  mkdir -p /home/user/build
+  yay -S --rebuildall --noconfirm --noprogressbar --builddir="/home/user/build" "$PACKAGE"
+EXU
 }
 
 get-env-vars() {
+  local AUR_PKG=$(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].PackageBase');
   # Get Env Vars
+  cd "/home/user/build/$AUR_PKG" || exit 1
   mkdir -p /build/; {
   echo "Package : $(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].PackageBase')"
   echo "Description : $(curl -s "https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=$PACKAGE" | jq -r '.results[0].Description')"
@@ -140,25 +86,14 @@ get-env-vars() {
   } >> /build/build.md
 }
 
-build (){
-  get-base-pkg "$PACKAGE"
-  # Build Package
-  cd "/home/user/$AUR_PKG" || exit 1
-  export USE_CCACHE=1
-  export CCACHE_EXEC=/usr/bin/ccache
-  ccache -M "$CCACHE_SIZE"
-  build-package
-}
-
 get-packages() {
-  get-base-pkg "$PACKAGE"
   # Copy compiled package
   mkdir -p /build/packages
-  find "/home/user/$AUR_PKG" -type f -name "*.pkg*" -exec cp -v {} "/build/packages" \;
+  find "/home/user/build" -type f -name "*.pkg*" -exec cp -v {} "/build/packages" \;
 }
+
 get-logs() {
   # Copy logs
-  get-base-pkg "$PACKAGE"
   mkdir -p /build/logs
   find "/home/user/$AUR_PKG" -type f -name "*.log" -exec cp -v {} "/build/logs" \; 2>/dev/null
   pr "check /build/logs for build logs"
